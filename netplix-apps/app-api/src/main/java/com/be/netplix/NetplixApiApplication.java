9890d87fae176f556cf7d6264c8facdbd4f2a310
hyunwoo.park <rebirth-howard@gmail.com>
2024-10-09 17:49:52 +0900

    멀티모듈 구조 및 api 서버 구동 세팅

---
+++ netplix-apps/app-api/src/main/java/com/be/netplix/NetplixApiApplication.java
@@ -0,0 +1,11 @@
+package com.be.netplix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetplixApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NetplixApiApplication.class);
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,15 @@
 rootProject.name = "netplix"
 
+include("netplix-adapters:adapter-http")
+include("netplix-adapters:adapter-persistence")
+
+include("netplix-apps:app-api")
+include("netplix-apps:app-batch")
+
+include("netplix-commons")
+
+include("netplix-core:core-domain")
+include("netplix-core:core-port")
+include("netplix-core:core-service")
+include("netplix-core:core-usecase")
+
