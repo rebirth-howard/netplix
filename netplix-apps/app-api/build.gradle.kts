3695ead0d49261bbcab7e589e27764739489a20a
hyunwoo.park <rebirth-howard@gmail.com>
2024-10-10 01:17:26 +0900

    스프링시큐리티 기초 설정

--- netplix-apps/app-api/build.gradle.kts
+++ netplix-apps/app-api/build.gradle.kts
@@ -1,21 +1,24 @@
 dependencies {
     implementation(project(":netplix-core:core-usecase"))
-    implementation(project(":netplix-adapters:adapter-http"))
-    implementation(project(":netplix-adapters:adapter-persistence"))
 
     runtimeOnly(project(":netplix-core:core-service"))
+    runtimeOnly(project(":netplix-adapters:adapter-http"))
+    runtimeOnly(project(":netplix-adapters:adapter-persistence"))
+    runtimeOnly(project(":netplix-adapters:adapter-redis"))
 
-
-//    implementation(project(":netplix-apps:app-batch"))
-//
-//    implementation(project(":netplix-adapters:adapter-http"))
-//    implementation(project(":netplix-adapters:adapter-persistence"))
-//
-//    implementation(project(":netplix-core:core-domain"))
-//    implementation(project(":netplix-core:core-port"))
-//    implementation(project(":netplix-core:core-service"))
-//    implementation(project(":netplix-core:core-usecase"))
-
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-aop")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation("org.springframework.security:spring-security-oauth2-client")
+    implementation("org.springframework.data:spring-data-commons")
 
     implementation("org.springframework.boot:spring-boot-starter-web")
+
+}
+
+val appMainClassName = "com.hw.netplix.NetplixApiApplication"
+tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+    mainClass.set(appMainClassName)
+    archiveClassifier.set("boot")
 }
